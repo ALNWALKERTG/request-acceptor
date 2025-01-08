db3a167428dabf08e8a924abd7903e195dc44eef
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2025-01-08 11:22:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ API_HASH = os.environ.get('API_HASH', '9dd30a06ae04f1b3fb44b02f22e99624')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '8057019885:AAHpn9T0XQv2NOB6kJqy_kI75YoMciHiPQY')
 PORT = os.environ.get("PORT", "8080")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1204889321 2144812475')]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001786472360'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002162564179'))
 
 # for mongodb
 DATABASE_NAME = os.environ.get("DB_NAME", "auto")     
